0abf87099154848f890172da358a312b58431ad6
Guilherme <redshotguii@outlook.com]>
2021-09-29 20:56:01 -0300

    Adiçao no codigo

--- Prova1bimestre/Prova1bimestre/Controllers/CalculoFreteController.cs
+++ Prova1bimestre/Prova1bimestre/Controllers/CalculoFreteController.cs
@@ -28,27 +28,33 @@ namespace Prova1bimestre.Controllers
        public ActionResult CalcularFrete(Dimensoes dimensoes)
         {
             
+            var F = (dimensoes.ComprimentoCm * dimensoes.LarguraCm * dimensoes.AlturaCm ) / 6000;
+
+            if (dimensoes.ComprimentoCm + dimensoes.LarguraCm + dimensoes.AlturaCm > 200)
+            {
+                return BadRequest("Valor da soma maior superior a 200");
+            }
 
             if (dimensoes.TipoPostagem == 1) 
             {
-                return Ok("Frete escolhido foi PAC :" + ((dimensoes.ComprimentoCm * dimensoes.LarguraCm * dimensoes.AlturaCm ) / 6000));
+                return Ok("Frete escolhido foi PAC :" + F);
             }
             if (dimensoes.TipoPostagem == 2)
             {
-                return Ok("Frete escolhido foi SEDEX :" + ((dimensoes.ComprimentoCm * dimensoes.LarguraCm * dimensoes.AlturaCm) * 10 / 6000 ));
+                return Ok("Frete escolhido foi SEDEX :" + (F * 10));
             }
             if (dimensoes.TipoPostagem == 3)
             {
-                return Ok("Frete escolhido foi SEDEX 10 :" + ((dimensoes.ComprimentoCm * dimensoes.LarguraCm * dimensoes.AlturaCm) * 20 / 6000 ));
+                return Ok("Frete escolhido foi SEDEX 10 :" + (F * 20));
             }
             if (dimensoes.TipoPostagem == 4)
             {
-                return Ok("Frete escolhido foi SEDEX 12 :" + ((dimensoes.ComprimentoCm * dimensoes.LarguraCm * dimensoes.AlturaCm) * 30 / 6000));
+                return Ok("Frete escolhido foi SEDEX 12 :" + (F * 30));
             }
 
 
             
-
+            
                 return BadRequest("Tipo de frete não informado");
         }
         
